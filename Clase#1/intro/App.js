f94385c9bab2c561dc1ddbb83210c97d024626f6
Esteban H <EstebanHernandez23201@hotmail.com>
2023-08-09 08:41:33 -0500

    Primer proyecto creado

--- Clase#1/intro/App.js
+++ Clase#1/intro/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Navbar } from './src/components/shared/Navbar/Navbar.jsx';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Navbar />
       <Text>Open up App.js to start working on your app!</Text>
-      <Text style={{color:'red'}}>Hello Native World</Text>
+      <Text style={{color:'yellow'}}>Hello Native World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff',
+    width:'100%',
+    height: '95%',
+    backgroundColor: 'cyan',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ Clase#1/intro/src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+
+import { View, Text } from "react-native";
+
+export const Navbar = () =>{
+    return(
+        <>
+        <View style={{width:'50vw', height: '10vh', backgroundColor:'cyan',padding:'5vw'}}>
+
+        <Text style={{textAlign: 'center', justifyContent: 'center'}}>Hello NavBar</Text>
+
+        </View>
+        </>
+        
+    );
+}
